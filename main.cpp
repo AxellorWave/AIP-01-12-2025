c152c0539a79a15d9208a956b1f0031f9b5d815a
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-12-01 17:18:35 +0300

    top dot pot etc.... DOTA2

--- main.cpp
+++ main.cpp
@@ -4,19 +4,40 @@ namespace top {
   };
 
   struct IDraw {
-    virtual p_t begin() = 0;
-    virtual p_t next() = 0;
+    virtual p_t begin() const = 0;
+    virtual p_t next() const = 0;
+  };
+
+  struct Dot: IDraw {
+    p_t o;
+    Dot(int x, int y);
+    p_t begin() const override;
+    p_t next() const override;
   };
 
   bool operator==(p_t a, p_t b)
   {
     return a.x == b.x && a.y == b.y;
   }
-
+  
   bool operator!=(p_t a, p_t b)
   {
     return !(a == b);
   }
+}
+
+top::Dot::Dot(int x, int y):
+  IDraw(), o{x,y}
+{}
+
+top::p_t top::Dot::begin() const
+{
+  return o;
+}
+
+top::p_t top::Dot::next() const
+{
+  return begin();
 }
 
 int main()
