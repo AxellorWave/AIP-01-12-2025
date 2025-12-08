3b9b675edeca051679165cb60fb5f27284c35e53
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-12-08 16:59:50 +0300

    update build_frame

--- main.cpp
+++ main.cpp
@@ -244,20 +244,26 @@ size_t top::get_points(IDraw * b, p_t ** ps, size_t & s)
     p = b->next(p);
     extend(ps, s + delta, p);
     ++delta;
-    //добавить p в массив
   }
   return delta;
-  // Достать точки
-  // Сгенерировать точки
-  // Положить в ps (обновить массив)
-  // Обновить размер
 }
 
 top::frame_t top::build_frame(const p_t * ps, size_t s)
 {
-  
-  // Найти min и max для x и y
-  // Сформировать frame_t
+  if (!s) {
+    throw std::logic_error("");
+  }
+  int minx = ps[0].x, maxx = ps[0].x;
+  int miny = ps[0].y, maxy = ps[0].y;
+  for (size_t i = 1; i < s; ++i) {
+    minx = std::min(minx, ps[i].x);
+    maxx = std::max(maxx, ps[i].x);
+    miny = std::min(miny, ps[i].y);
+    maxy = std::max(maxy, ps[i].y);
+  }
+  p_t aa {minx, miny};
+  p_t bb { maxx, maxy};
+  return {aa, bb};
 }
 
 char * top::build_canvas(frame_t f)
