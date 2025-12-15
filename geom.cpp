871a105051b344139a87a6b4ce670e8295341a6a
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-12-15 16:56:36 +0300

    update files geom

--- geom.cpp
+++ geom.cpp
@@ -18,3 +18,21 @@ size_t top::cols(frame_t fr)
 {
   return (fr.rightTop.x - fr.leftBot.x + 1);
 }
+
+top::frame_t top::build_frame(const p_t * ps, size_t s)
+{
+  if (!s) {
+    throw std::logic_error("");
+  }
+  int minx = ps[0].x, maxx = ps[0].x;
+  int miny = ps[0].y, maxy = ps[0].y;
+  for (size_t i = 1; i < s; ++i) {
+    minx = std::min(minx, ps[i].x);
+    maxx = std::max(maxx, ps[i].x);
+    miny = std::min(miny, ps[i].y);
+    maxy = std::max(maxy, ps[i].y);
+  }
+  p_t aa {minx, miny};
+  p_t bb { maxx, maxy};
+  return {aa, bb};
+}
--- geom.hpp
+++ geom.hpp
@@ -1,6 +1,8 @@
 #ifndef GEOM_HPP
 #define GEOM_HPP
 #include <cstddef>
+#include <stdexcept>
+
 namespace top {
   struct p_t {
     int x, y;
@@ -9,7 +11,7 @@ namespace top {
   struct frame_t {
     p_t leftBot, rightTop;
   };
-
+  frame_t build_frame(const p_t * ps, size_t s);
   size_t rows(frame_t fr);
   size_t cols(frame_t fr);
   bool operator==(p_t a, p_t b);
