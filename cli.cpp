5fd9fe074e117bf346ed6ecd2005bc4abbf27f74
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-12-25 15:22:00 +0300

    cli with strs

--- cli.cpp
+++ cli.cpp
@@ -5,16 +5,27 @@ void hi()
   std::cout << "< Hi! >\n";
 }
 
+bool is_space(char c)
+{
+  return c == ' ';
+}
+
+std::istream& getword(std::istream& is, char * word, size_t k, bool(*c)(char));
+
+size_t match(const char * word , const char * const * words, size_t k);
+
 int main()
 {
   constexpr size_t cmds_counts = 1;
   void(*cmds[1])() = {hi};
-  size_t i = 0;
-  while (!(std::cin >> i).eof()) {
+  const char * const cmds_text[] = {"hi"};
+  constexpr size_t bsize = 255;
+  char word[bsize + 1] = {};
+  while (!getword(std::cin, word, bsize, is_space)) {
     if (std::cin.fail()) {
       std::cerr << "< INVALID INPUT >\n";
       return 1;
-    } else if (i < cmds_counts) {
+    } else if (size_t i = match(word, cmds_text, cmds_counts); i < cmds_counts) {
       cmds[i]();
     } else {
       std::cerr << "< INVALID COMMAND >\n";
